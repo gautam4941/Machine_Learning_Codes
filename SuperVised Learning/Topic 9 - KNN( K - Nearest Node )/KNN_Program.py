4ce935424fe1e1b25505b0c5877d666327f3f605
Kumar Gautam Pathak <36535562+gautam4941@users.noreply.github.com>
2021-04-07 11:58:03 +0530

    Add files via upload

--- SuperVised Learning/Topic 9 - KNN( K - Nearest Node )/KNN_Program.py
+++ SuperVised Learning/Topic 9 - KNN( K - Nearest Node )/KNN_Program.py
@@ -20,15 +20,59 @@ print( f"x.dtypes :- \n{ x.dtypes }\n" )
 print( f"y.dtypes :- \n{ y.dtypes }\n" )
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0 )
 
-print( f"x_train :- \n{ x_train }\n" )
-print( f"y_train :- \n{ y_train }\n" )
-print( f"x_test :- \n{ x_test }\n" )
-print( f"y_test :- \n{ y_test }\n" )
+max_acc = 0
+max_i = -1
+max_n = -1
+
+for i in range( 0, 21 ):	#i = 0, 1
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = i )
+	
+	for j in range( 1, 8 ):		#i= 0 , j = 1
+		model = KNeighborsClassifier( n_neighbors = j )		#model = KNeighborsClassifier( n_neighbors = j )
+		model.fit( x_train, y_train)
+		y_pred = model.predict( x_test )
+		accuracy = accuracy_score( y_test, y_pred )
+		
+		print( f"i = {i} and j = {j} and accuracy :- { accuracy }" )
+		
+		if( accuracy > max_acc ):
+			max_acc = accuracy
+			max_i = i
+			max_n = j
+			
+			
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = max_i ) #and K = 4
+			
+error_rate = []
+acc_rate = []
+temp = x_test.copy()			#temp.row = 30
+temp['y_test'] = y_test			#temp.row = 30
+
+for i in range(1,40):		#1, 2, ..., 39
+ model = KNeighborsClassifier( n_neighbors = i )
+ model.fit( x_train, y_train)
+ y_pred = model.predict(x_test)		#x_test.rows = 30 and y_pred.row = 30
+ temp['y_pred'] = y_pred			#temp.row = 30
+ 
+ error_rate.append( np.mean( temp['y_test'] != temp['y_pred'] ) )
+ acc_rate.append( np.mean( temp['y_test'] == temp['y_pred'] ) )
+
+import matplotlib.pyplot as plt
+
+
+plt.plot(np.arange( 1, len(error_rate) + 1 ),error_rate,color='red', linestyle='dashed', 
+         marker='o',markerfacecolor='orange', markersize=10, label = 'Error' )
+		 
+plt.plot( np.arange( 1, len(error_rate) + 1 ),acc_rate,color='green', linestyle='dashed', 
+         marker='*',markerfacecolor='blue', markersize=10, label = 'Accuracy' )
+plt.title('Error Rate vs. K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
+plt.show()
 
 from sklearn.neighbors import KNeighborsClassifier
-model = KNeighborsClassifier( n_neighbors = 5 )
+model = KNeighborsClassifier( n_neighbors = ? )
 model.fit( x_train, y_train)
 y_predict = model.predict( x_test )
 
